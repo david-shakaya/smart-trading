e6904f722d0205d6815c13051ddee9b846f3b386
david-shakaya <devidoz666@gmail.com>
2021-09-19 13:41:21 +0300

    added overlay

--- src/js/header-burger.js
+++ src/js/header-burger.js
@@ -1,9 +1,14 @@
 // toggleClass('open');
 
 const burger = document.querySelector('.burger-js');
+const body = document.querySelector('body');
+const overlay = document.querySelector('.overlay-js')
 
 burger.addEventListener('click', fn);
 
 function fn(e) {
+  body.classList.toggle('invisible')
+  overlay.classList.toggle('overlay')
   burger.classList.toggle('open');
+
 }
